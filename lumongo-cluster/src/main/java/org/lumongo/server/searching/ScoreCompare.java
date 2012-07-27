c09c5009e784b1a60d7d8fb91007e501b7de9a02
Matt Davis <davis.matt.t@gmail.com>
2012-07-27 12:24:14 +0000

    reversed compare

--- lumongo-cluster/src/main/java/org/lumongo/server/searching/ScoreCompare.java
+++ lumongo-cluster/src/main/java/org/lumongo/server/searching/ScoreCompare.java
@@ -1,18 +1,22 @@
 package org.lumongo.server.searching;
 
+
 import java.util.Comparator;
+
 
 import org.lumongo.cluster.message.Lumongo.ScoredResult;
 
 public class ScoreCompare implements Comparator<ScoredResult> {
     @Override
     public int compare(ScoredResult o1, ScoredResult o2) {
-        int compare = Double.compare(o1.getScore(), o2.getScore());
+        int compare = Double.compare(o1.getScore(), o2.getScore()) * -1;
         if (compare == 0) {
             return Integer.compare(o1.getResultIndex(), o2.getResultIndex());
         }
         return compare;
     }
+    
+
 }
 
 
