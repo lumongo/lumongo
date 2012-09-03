ef15a3b69f4ffe43faf5c9d90e1dec86782cf104
Matt Davis <davis.matt.t@gmail.com>
2012-09-03 16:52:47 +0000

    add more annotations to make a more annotations driven storage

---
+++ lumongo-common/src/main/java/org/lumongo/fields/UniqueIdFieldInfo.java
@@ -0,0 +1,31 @@
+package org.lumongo.fields;
+
+import java.lang.reflect.Field;
+
+public class UniqueIdFieldInfo<T> {
+    private final String fieldName;
+    private final Field field;
+
+    public UniqueIdFieldInfo(Field field, String fieldName) {
+        this.fieldName = fieldName;
+        this.field = field;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public String build(T object) throws IllegalArgumentException, IllegalAccessException {
+        if (object != null) {
+            Object o = field.get(object);
+
+            if (o instanceof String) {
+                return (String) o;
+            }
+
+        }
+        throw new RuntimeException("Unique id field <" + field.getName() + "> must not be null");
+    }
+
+
+}
