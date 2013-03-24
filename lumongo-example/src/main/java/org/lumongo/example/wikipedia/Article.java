36a4a7408e2032812956ab17d2a0704a7b818dbf
Matt Davis <davis.matt.t@gmail.com>
2013-03-24 22:32:14 +0000

    fix this code, got removed

---
+++ lumongo-example/src/main/java/org/lumongo/example/wikipedia/Article.java
@@ -0,0 +1,128 @@
+package org.lumongo.example.wikipedia;
+
+import java.util.Date;
+
+import org.lumongo.cluster.message.Lumongo.LMAnalyzer;
+import org.lumongo.fields.annotations.DefaultSearch;
+import org.lumongo.fields.annotations.Faceted;
+import org.lumongo.fields.annotations.Indexed;
+import org.lumongo.fields.annotations.Saved;
+import org.lumongo.fields.annotations.Settings;
+import org.lumongo.fields.annotations.UniqueId;
+
+
+@Settings(
+        indexName = "wikipedia",
+        numberOfSegments = 16,
+        segmentFlushInterval = 1000,
+        segmentCommitInterval = 6000
+        )
+public class Article {
+
+    public Article() {
+
+    }
+
+    @UniqueId
+    private String id;
+
+    @Saved
+    @Indexed(LMAnalyzer.STANDARD)
+    private String title;
+
+    @Saved
+    @Indexed(LMAnalyzer.NUMERIC_INT)
+    private Integer namespace;
+
+
+    @DefaultSearch
+    //@Saved(compressed = true)
+    @Saved
+    @Indexed(LMAnalyzer.STANDARD)
+    private String text;
+
+    @Saved
+    @Indexed(LMAnalyzer.NUMERIC_LONG)
+    private Long revision;
+
+    @Saved
+    @Indexed(LMAnalyzer.NUMERIC_INT)
+    private Integer userId;
+
+    @Saved
+    @Faceted
+    @Indexed(LMAnalyzer.STANDARD)
+    private String user;
+
+    @Saved
+    @Indexed(LMAnalyzer.NUMERIC_LONG)
+    private Date revisionDate;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getNamespace() {
+        return namespace;
+    }
+
+    public void setNamespace(Integer namespace) {
+        this.namespace = namespace;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Long getRevision() {
+        return revision;
+    }
+
+    public void setRevision(Long revision) {
+        this.revision = revision;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public Date getRevisionDate() {
+        return revisionDate;
+    }
+
+    public void setRevisionDate(Date revisionDate) {
+        this.revisionDate = revisionDate;
+    }
+
+
+
+}
---
+++ lumongo-example/src/main/java/org/lumongo/example/wikipedia/IndexWikipedia.java
@@ -0,0 +1,139 @@
+package org.lumongo.example.wikipedia;
+
+import info.bliki.wiki.filter.PlainTextConverter;
+import info.bliki.wiki.model.WikiModel;
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.Future;
+
+import org.apache.log4j.Logger;
+import org.lumongo.client.command.Store;
+import org.lumongo.client.config.LumongoPoolConfig;
+import org.lumongo.client.pool.LumongoWorkPool;
+import org.lumongo.client.result.CreateOrUpdateIndexResult;
+import org.lumongo.client.result.StoreResult;
+import org.lumongo.fields.Mapper;
+import org.lumongo.util.LogUtil;
+import org.lumongo.xml.StaxJAXBReader;
+import org.wikipedia.schema.ContributorType;
+import org.wikipedia.schema.PageType;
+import org.wikipedia.schema.RedirectType;
+import org.wikipedia.schema.RevisionType;
+import org.wikipedia.schema.TextType;
+
+
+public class IndexWikipedia {
+
+    @SuppressWarnings("unused")
+    private final static Logger log = Logger.getLogger(IndexWikipedia.class);
+
+    private static Mapper<Article> mapper;
+
+    private static LumongoWorkPool lumongoWorkPool;
+
+    private static WikiModel wikiModel = new WikiModel("/wiki/${image}", "/wiki/${title}");
+    private static PlainTextConverter plainTextConverter = new PlainTextConverter();
+
+    public static void main(String[] args) throws Exception {
+
+    	if (args.length != 1) {
+    		System.out.println("Usage: filename");
+    		System.out.println(" ex. /tmp/enwiki-20120802-pages-articles-multistream.xml");
+    		System.out.println(" download from http://dumps.wikimedia.org/enwiki/20120802/enwiki-20120802-pages-articles-multistream.xml.bz2");
+    		System.exit(1);
+    	}
+
+    	String fileName = args[0];
+
+    	if (!(new File(fileName)).exists()) {
+    		System.out.println("File <" + fileName + "> does not exist");
+    		System.exit(2);
+    	}
+
+    	LogUtil.loadLogConfig();
+
+
+        lumongoWorkPool = new LumongoWorkPool(new LumongoPoolConfig().addMember("localhost"));
+        mapper = new Mapper<Article>(Article.class);
+
+        @SuppressWarnings("unused")
+        CreateOrUpdateIndexResult createOrUpdateIndexResult = lumongoWorkPool.createOrUpdateIndex(mapper.createOrUpdateIndex());
+
+        StaxJAXBReader<PageType> s = new StaxJAXBReader<PageType>(PageType.class, "page") {
+
+            private int counter = 0;
+            private long start = System.currentTimeMillis();
+            private long last = System.currentTimeMillis();
+
+            @Override
+            public void handle(PageType item) throws Exception {
+                final Article article = formArticle(item);
+                if (article != null) {
+
+                    Store store = mapper.createStore(article);
+
+                    @SuppressWarnings("unused")
+                    Future<StoreResult> sr = lumongoWorkPool.storeAsync(store);
+
+                }
+                if (++counter % 5000 == 0) {
+                    long end = System.currentTimeMillis();
+                    long timeForSet = end - last;
+                    long timeSinceStart = end - start;
+                    System.out.println(counter + "\t" + timeForSet + "\t" + timeSinceStart);
+                    last = end;
+                }
+            }
+
+        };
+
+        s.handleFile(fileName);
+
+        lumongoWorkPool.shutdown();
+    }
+
+    public static Article formArticle(final PageType page) {
+        RedirectType redirectType = page.getRedirect();
+        if (redirectType != null) {
+            // skip redirects
+            return null;
+        }
+
+        Article article = new Article();
+        article.setTitle(page.getTitle());
+        article.setNamespace(page.getNs().intValue());
+        article.setId(page.getId().toString());
+
+        List<Object> revUploadList = page.getRevisionOrUpload();
+
+        for (Object o : revUploadList) {
+            if (o instanceof RevisionType) {
+                RevisionType revisionType = (RevisionType) o;
+
+                TextType textType = revisionType.getText();
+                String wikiText = textType.getValue();
+                //convert wiki markup to plain text for simplicity
+                String plainText = wikiModel.render(plainTextConverter, wikiText);
+                article.setText(plainText);
+                article.setRevision(revisionType.getId().longValue());
+
+                ContributorType contributorType = revisionType.getContributor();
+                if (contributorType != null) {
+                    if (contributorType.getId() != null) {
+                        article.setUserId(contributorType.getId().intValue());
+                    }
+                    article.setUser(contributorType.getUsername());
+                }
+                article.setRevisionDate(revisionType.getTimestamp().toGregorianCalendar().getTime());
+
+                break;
+            }
+        }
+        return article;
+    }
+
+
+
+
+}
