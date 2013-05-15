a8a2c1beb72f32f08bcd1d0e1f41fa54021f59ed
Matt Davis <davis.matt.t@gmail.com>
2013-05-15 22:48:03 +0000

    add server option

--- lumongo-example/src/main/java/org/lumongo/example/wikipedia/IndexWikipedia.java
+++ lumongo-example/src/main/java/org/lumongo/example/wikipedia/IndexWikipedia.java
@@ -25,113 +25,124 @@ import org.wikipedia.schema.TextType;
 
 public class IndexWikipedia {
 
-    @SuppressWarnings("unused")
-    private final static Logger log = Logger.getLogger(IndexWikipedia.class);
+	@SuppressWarnings("unused")
+	private final static Logger log = Logger.getLogger(IndexWikipedia.class);
 
-    private static Mapper<Article> mapper;
+	private static Mapper<Article> mapper;
 
-    private static LumongoWorkPool lumongoWorkPool;
+	private static LumongoWorkPool lumongoWorkPool;
 
-    private static WikiModel wikiModel = new WikiModel("/wiki/${image}", "/wiki/${title}");
-    private static PlainTextConverter plainTextConverter = new PlainTextConverter();
+	private static WikiModel wikiModel = new WikiModel("/wiki/${image}", "/wiki/${title}");
+	private static PlainTextConverter plainTextConverter = new PlainTextConverter();
 
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-    	if (args.length != 1) {
-    		System.out.println("Usage: filename");
-    		System.out.println(" ex. /tmp/enwiki-20120802-pages-articles-multistream.xml");
-    		System.out.println(" download from http://dumps.wikimedia.org/enwiki/20120802/enwiki-20120802-pages-articles-multistream.xml.bz2");
-    		System.exit(1);
-    	}
+		if (args.length != 2) {
+			System.out.println("Usage: filename lumongoServers");
+			System.out.println(" ex. /tmp/enwiki-20120802-pages-articles-multistream.xml 10.0.0.10,10.0.0.11");
+			System.out.println(" download from http://dumps.wikimedia.org/enwiki/20120802/enwiki-20120802-pages-articles-multistream.xml.bz2");
+			System.out.println(" a single active lumongo server is enough, cluster membership will be updated when a connection is established");
+			System.exit(1);
+		}
 
-    	String fileName = args[0];
+		String fileName = args[0];
+		String[] servers = args[1].split(",");
 
-    	if (!(new File(fileName)).exists()) {
-    		System.out.println("File <" + fileName + "> does not exist");
-    		System.exit(2);
-    	}
+		if (!(new File(fileName)).exists()) {
+			System.out.println("File <" + fileName + "> does not exist");
+			System.exit(2);
+		}
 
-    	LogUtil.loadLogConfig();
+		LogUtil.loadLogConfig();
 
 
-        lumongoWorkPool = new LumongoWorkPool(new LumongoPoolConfig().addMember("localhost"));
-        mapper = new Mapper<Article>(Article.class);
 
-        @SuppressWarnings("unused")
-        CreateOrUpdateIndexResult createOrUpdateIndexResult = lumongoWorkPool.createOrUpdateIndex(mapper.createOrUpdateIndex());
+		LumongoPoolConfig lumongoPoolConfig = new LumongoPoolConfig();
+		lumongoPoolConfig.setDefaultRetries(servers.length - 1); //?
+		for (String server : servers) {
+			lumongoPoolConfig.addMember(server);
+		}
+		lumongoWorkPool = new LumongoWorkPool(lumongoPoolConfig);
+		lumongoWorkPool.updateMembers();
 
-        StaxJAXBReader<PageType> s = new StaxJAXBReader<PageType>(PageType.class, "page") {
 
-            private int counter = 0;
-            private long start = System.currentTimeMillis();
-            private long last = System.currentTimeMillis();
+		mapper = new Mapper<Article>(Article.class);
 
-            @Override
-            public void handle(PageType item) throws Exception {
-                final Article article = formArticle(item);
-                if (article != null) {
+		@SuppressWarnings("unused")
+		CreateOrUpdateIndexResult createOrUpdateIndexResult = lumongoWorkPool.createOrUpdateIndex(mapper.createOrUpdateIndex());
 
-                    Store store = mapper.createStore(article);
+		StaxJAXBReader<PageType> s = new StaxJAXBReader<PageType>(PageType.class, "page") {
 
-                    @SuppressWarnings("unused")
-                    Future<StoreResult> sr = lumongoWorkPool.storeAsync(store);
+			private int counter = 0;
+			private long start = System.currentTimeMillis();
+			private long last = System.currentTimeMillis();
 
-                }
-                if (++counter % 5000 == 0) {
-                    long end = System.currentTimeMillis();
-                    long timeForSet = end - last;
-                    long timeSinceStart = end - start;
-                    System.out.println(counter + "\t" + timeForSet + "\t" + timeSinceStart);
-                    last = end;
-                }
-            }
+			@Override
+			public void handle(PageType item) throws Exception {
+				final Article article = formArticle(item);
+				if (article != null) {
 
-        };
+					Store store = mapper.createStore(article);
 
-        s.handleFile(fileName);
+					@SuppressWarnings("unused")
+					Future<StoreResult> sr = lumongoWorkPool.storeAsync(store);
 
-        lumongoWorkPool.shutdown();
-    }
+				}
+				if (++counter % 5000 == 0) {
+					long end = System.currentTimeMillis();
+					long timeForSet = end - last;
+					long timeSinceStart = end - start;
+					System.out.println(counter + "\t" + timeForSet + "\t" + timeSinceStart);
+					last = end;
+				}
+			}
 
-    public static Article formArticle(final PageType page) {
-        RedirectType redirectType = page.getRedirect();
-        if (redirectType != null) {
-            // skip redirects
-            return null;
-        }
+		};
 
-        Article article = new Article();
-        article.setTitle(page.getTitle());
-        article.setNamespace(page.getNs().intValue());
-        article.setId(page.getId().toString());
+		s.handleFile(fileName);
 
-        List<Object> revUploadList = page.getRevisionOrUpload();
+		lumongoWorkPool.shutdown();
+	}
 
-        for (Object o : revUploadList) {
-            if (o instanceof RevisionType) {
-                RevisionType revisionType = (RevisionType) o;
+	public static Article formArticle(final PageType page) {
+		RedirectType redirectType = page.getRedirect();
+		if (redirectType != null) {
+			// skip redirects
+			return null;
+		}
 
-                TextType textType = revisionType.getText();
-                String wikiText = textType.getValue();
-                //convert wiki markup to plain text for simplicity
-                String plainText = wikiModel.render(plainTextConverter, wikiText);
-                article.setText(plainText);
-                article.setRevision(revisionType.getId().longValue());
+		Article article = new Article();
+		article.setTitle(page.getTitle());
+		article.setNamespace(page.getNs().intValue());
+		article.setId(page.getId().toString());
 
-                ContributorType contributorType = revisionType.getContributor();
-                if (contributorType != null) {
-                    if (contributorType.getId() != null) {
-                        article.setUserId(contributorType.getId().intValue());
-                    }
-                    article.setUser(contributorType.getUsername());
-                }
-                article.setRevisionDate(revisionType.getTimestamp().toGregorianCalendar().getTime());
+		List<Object> revUploadList = page.getRevisionOrUpload();
 
-                break;
-            }
-        }
-        return article;
-    }
+		for (Object o : revUploadList) {
+			if (o instanceof RevisionType) {
+				RevisionType revisionType = (RevisionType) o;
+
+				TextType textType = revisionType.getText();
+				String wikiText = textType.getValue();
+				//convert wiki markup to plain text for simplicity
+				String plainText = wikiModel.render(plainTextConverter, wikiText);
+				article.setText(plainText);
+				article.setRevision(revisionType.getId().longValue());
+
+				ContributorType contributorType = revisionType.getContributor();
+				if (contributorType != null) {
+					if (contributorType.getId() != null) {
+						article.setUserId(contributorType.getId().intValue());
+					}
+					article.setUser(contributorType.getUsername());
+				}
+				article.setRevisionDate(revisionType.getTimestamp().toGregorianCalendar().getTime());
+
+				break;
+			}
+		}
+		return article;
+	}
 
 
 
