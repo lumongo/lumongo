660f8bb82b356c04a62eb25f289ef29dfed97dd0
Matt Davis <davis.matt.t@gmail.com>
2013-09-22 14:15:26 +0000

    add long and date to indexeddocbuilder

--- lumongo-client/src/main/java/org/lumongo/doc/IndexedDocBuilder.java
+++ lumongo-client/src/main/java/org/lumongo/doc/IndexedDocBuilder.java
@@ -1,77 +1,96 @@
 package org.lumongo.doc;
 
+import java.util.Date;
+
 import org.lumongo.LumongoConstants;
 import org.lumongo.cluster.message.Lumongo.LMDoc;
 import org.lumongo.cluster.message.Lumongo.LMField;
 import org.lumongo.util.StringUtil;
 
 public class IndexedDocBuilder {
-
+	
 	private LMDoc.Builder indexedDocBuilder;
-
+	
 	public static IndexedDocBuilder newBuilder() {
 		return new IndexedDocBuilder();
 	}
-
+	
 	public IndexedDocBuilder() {
 		indexedDocBuilder = LMDoc.newBuilder();
 	}
-
+	
 	public IndexedDocBuilder addField(String fieldName, String... values) {
-
+		
 		for (String value : values) {
 			indexedDocBuilder.addIndexedField(LMField.newBuilder().setFieldName(fieldName).addFieldValue(value));
 		}
-
+		
 		return this;
 	}
-
+	
 	public IndexedDocBuilder addField(String fieldName, Integer... values) {
-
+		
 		for (Integer value : values) {
 			indexedDocBuilder.addIndexedField(LMField.newBuilder().setFieldName(fieldName).addIntValue(value));
 		}
-
+		
 		return this;
 	}
-
+	
+	public IndexedDocBuilder addField(String fieldName, Long... values) {
+		
+		for (Long value : values) {
+			indexedDocBuilder.addIndexedField(LMField.newBuilder().setFieldName(fieldName).addLongValue(value));
+		}
+		
+		return this;
+	}
+	
 	public IndexedDocBuilder addField(String fieldName, Float... values) {
-
+		
 		for (Float value : values) {
 			indexedDocBuilder.addIndexedField(LMField.newBuilder().setFieldName(fieldName).addFloatValue(value));
 		}
 		return this;
 	}
-
+	
 	public IndexedDocBuilder addField(String fieldName, Double... values) {
-
+		
 		for (Double value : values) {
 			indexedDocBuilder.addIndexedField(LMField.newBuilder().setFieldName(fieldName).addDoubleValue(value));
 		}
 		return this;
 	}
-
+	
+	public IndexedDocBuilder addField(String fieldName, Date... values) {
+		
+		for (Date value : values) {
+			indexedDocBuilder.addIndexedField(LMField.newBuilder().setFieldName(fieldName).addLongValue(value.getTime()));
+		}
+		return this;
+	}
+	
 	public void addFacet(String... path) {
 		indexedDocBuilder.addFacet(StringUtil.join(LumongoConstants.FACET_DELIMITER, path));
 	}
-
+	
 	public IndexedDocBuilder clearFields() {
 		indexedDocBuilder.clearIndexedField();
 		return this;
 	}
-
+	
 	public IndexedDocBuilder clearFacets() {
 		indexedDocBuilder.clearFacet();
 		return this;
 	}
-
+	
 	public IndexedDocBuilder clear() {
 		indexedDocBuilder.clear();
 		return this;
 	}
-
+	
 	public LMDoc getIndexedDoc() {
 		return indexedDocBuilder.build();
 	}
-
+	
 }
