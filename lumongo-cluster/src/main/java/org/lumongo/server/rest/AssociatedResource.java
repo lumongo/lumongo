c6b64daefcc533cb4e9b3702f333b91ab35b23bf
Matt Davis <davis.matt.t@gmail.com>
2014-06-04 19:55:33 -0400

    start switch to grizzly for the REST service

---
+++ lumongo-cluster/src/main/java/org/lumongo/server/rest/AssociatedResource.java
@@ -0,0 +1,80 @@
+package org.lumongo.server.rest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+
+import org.lumongo.LumongoConstants;
+import org.lumongo.server.indexing.IndexManager;
+import org.lumongo.util.StreamHelper;
+
+@Path(LumongoConstants.ASSOCIATED_DOCUMENTS_URL)
+public class AssociatedResource {
+	
+	private IndexManager indexManager;
+	
+	public AssociatedResource(IndexManager indexManager) {
+		this.indexManager = indexManager;
+	}
+	
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public StreamingOutput get(@QueryParam(LumongoConstants.UNIQUE_ID) final String uniqueId, @QueryParam(LumongoConstants.FILE_NAME) final String fileName,
+					@QueryParam(LumongoConstants.INDEX_NAME) final String indexName) {
+		return new StreamingOutput() {
+			
+			@Override
+			public void write(OutputStream output) throws IOException, WebApplicationException {
+				if (uniqueId != null && fileName != null && indexName != null) {
+					InputStream is = indexManager.getAssociatedDocumentStream(indexName, uniqueId, fileName);
+					if (is != null) {
+						StreamHelper.copyStream(is, output);
+						
+					}
+					else {
+						throw new WebApplicationException("Cannot find associated document with uniqueId <" + uniqueId + "> with fileName <" + fileName + ">",
+										LumongoConstants.NOT_FOUND);
+					}
+				}
+				else {
+					throw new WebApplicationException(LumongoConstants.UNIQUE_ID + " and " + LumongoConstants.FILE_NAME + " are required",
+									LumongoConstants.BAD_REQUEST);
+				}
+			}
+			
+		};
+		
+	}
+	
+	@POST
+	@Produces({ MediaType.TEXT_XML })
+	public Response post(@QueryParam(LumongoConstants.UNIQUE_ID) final String uniqueId, @QueryParam(LumongoConstants.FILE_NAME) final String fileName,
+					@QueryParam(LumongoConstants.INDEX_NAME) final String indexName, final InputStream is) {
+		if (uniqueId != null && fileName != null && indexName != null) {
+			
+			try {
+				indexManager.storeAssociatedDocument(indexName, uniqueId, fileName, is, false, null);
+				
+				return Response.status(LumongoConstants.SUCCESS)
+								.entity("Stored associated document with uniqueId <" + uniqueId + "> and fileName <" + fileName + ">").build();
+			}
+			catch (Exception e) {
+				return Response.status(LumongoConstants.INTERNAL_ERROR).entity(e.getMessage()).build();
+			}
+		}
+		else {
+			throw new WebApplicationException(LumongoConstants.UNIQUE_ID + " and " + LumongoConstants.FILE_NAME + " are required", LumongoConstants.BAD_REQUEST);
+		}
+		
+	}
+}
--- lumongo-cluster/src/main/java/org/lumongo/server/rest/RestServiceManager.java
+++ lumongo-cluster/src/main/java/org/lumongo/server/rest/RestServiceManager.java
@@ -1,40 +1,46 @@
 package org.lumongo.server.rest;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+import javax.ws.rs.core.UriBuilder;
 
 import org.apache.log4j.Logger;
-import org.lumongo.LumongoConstants;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.lumongo.server.config.LocalNodeConfig;
 import org.lumongo.server.indexing.IndexManager;
-
-import com.sun.net.httpserver.HttpServer;
 
 public class RestServiceManager {
 	
 	private final static Logger log = Logger.getLogger(RestServiceManager.class);
 	
 	private final int restPort;
-	private HttpServer httpServer;
 	
-	private IndexManager indexManger;
+	private IndexManager indexManager;
+	
+	private HttpServer server;
 	
 	public RestServiceManager(LocalNodeConfig localNodeConfig, IndexManager indexManager) {
-		this.indexManger = indexManager;
+		this.indexManager = indexManager;
 		this.restPort = localNodeConfig.getRestPort();
 		
 	}
 	
 	public void start() throws IOException {
-		InetSocketAddress address = new InetSocketAddress(restPort);
-		httpServer = HttpServer.create(address, 0);
-		httpServer.start();
-		httpServer.createContext(LumongoConstants.ASSOCIATED_DOCUMENTS_URL, new AssociatedHandler(indexManger));
+		
+		URI baseUri = UriBuilder.fromUri("http://0.0.0.0/").port(restPort).build();
+		ResourceConfig config = new ResourceConfig();
+		config.register(new AssociatedResource(indexManager));
+		server = GrizzlyHttpServerFactory.createHttpServer(baseUri, config);
+		
 	}
 	
 	public void shutdown() {
 		log.info("Starting rest service shutdown");
 		//TODO configure
-		httpServer.stop(1);
+		server.shutdown(1, TimeUnit.SECONDS);
 	}
 }
