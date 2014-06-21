8b8d580de0565a3da674cb93c295f1df38ccfc1b
Matt Davis <davis.matt.t@gmail.com>
2014-06-21 17:41:31 -0400

    make tests default to localhost and 27017

--- lumongo-storage/src/main/java/org/lumongo/util/TestHelper.java
+++ lumongo-storage/src/main/java/org/lumongo/util/TestHelper.java
@@ -9,23 +9,26 @@ public class TestHelper {
 	public static final String MONGO_SERVER_PROPERTY = "mongoServer";
 	public static final String MONGO_PORT_PROPERTY = "mongoPort";
 	public static final String TEST_DATABASE_NAME = "lumongoUnitTest";
-
+	
+	public static final String MONGO_SERVER_PROPERTY_DEFAULT = "127.0.0.1";
+	public static final int MONGO_PORT_PROPERTY_DEFAULT = 27017;
+	
 	public static String getMongoServer() {
 		String mongoServer = System.getProperty(MONGO_SERVER_PROPERTY);
 		if (mongoServer == null) {
-			throw new IllegalArgumentException(MONGO_SERVER_PROPERTY + " must be defined");
+			return MONGO_SERVER_PROPERTY_DEFAULT;
 		}
 		return mongoServer;
 	}
-
+	
 	public static int getMongoPort() {
 		String portStr = System.getProperty(MONGO_PORT_PROPERTY);
 		if (portStr == null) {
-			throw new IllegalArgumentException(MONGO_PORT_PROPERTY + " must be defined");
+			return MONGO_PORT_PROPERTY_DEFAULT;
 		}
 		return Integer.parseInt(portStr);
 	}
-
+	
 	public static MongoClient getMongo() throws UnknownHostException, MongoException {
 		return new MongoClient(getMongoServer(), getMongoPort());
 	}
