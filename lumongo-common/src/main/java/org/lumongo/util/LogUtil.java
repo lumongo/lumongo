c77c45afcb7dcb3d1e2566f155494cb765ff7b07
Matt Davis <davis.matt.t@gmail.com>
2014-07-17 12:32:50 -0400

    drop the loglevel.properties file and just configure by code

--- lumongo-common/src/main/java/org/lumongo/util/LogUtil.java
+++ lumongo-common/src/main/java/org/lumongo/util/LogUtil.java
@@ -1,8 +1,9 @@
 package org.lumongo.util;
 
-import java.net.URL;
-
-import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 
 public class LogUtil {
 	
@@ -18,12 +19,21 @@ public class LogUtil {
 		synchronized (lock) {
 			
 			if (!loaded) {
-				String propPath = "/etc/loglevel.properties";
-				URL url = LogUtil.class.getResource(propPath);
-				if (url == null) {
-					throw new Exception("Cannot find log properties file: " + propPath);
-				}
-				PropertyConfigurator.configure(url);
+				ConsoleAppender console = new ConsoleAppender(); //create appender
+				//configure the appender
+				String PATTERN = "%d [%t] <%p> %c{2}: %m%n";
+				console.setLayout(new PatternLayout(PATTERN));
+				console.setThreshold(Level.INFO);
+				console.activateOptions();
+				//add appender to any Logger (here is root)
+				Logger.getRootLogger().addAppender(console);
+				
+				//String propPath = "/etc/loglevel.properties";
+				//URL url = LogUtil.class.getResource(propPath);
+				//if (url == null) {
+				//	throw new Exception("Cannot find log properties file: " + propPath);
+				//}
+				//PropertyConfigurator.configure(url);
 				loaded = true;
 			}
 		}
