4615dd01e7002f9c85ef6d3c9a87379a7b3d4f51
Matt Davis <matt.davis@temis.us>
2014-07-19 21:37:54 -0400

    add fields to rest service for query field and filter query

--- lumongo-cluster/src/main/java/org/lumongo/server/rest/QueryResource.java
+++ lumongo-cluster/src/main/java/org/lumongo/server/rest/QueryResource.java
@@ -22,46 +22,65 @@ import com.googlecode.protobuf.format.JsonFormat;
 
 @Path(LumongoConstants.QUERY_URL)
 public class QueryResource {
-	
+
 	private LumongoIndexManager indexManager;
-	
+
 	public QueryResource(LumongoIndexManager indexManager) {
 		this.indexManager = indexManager;
-		
+
 	}
-	
+
 	@GET
 	@Produces({ MediaType.APPLICATION_JSON })
-	public String get(@QueryParam(LumongoConstants.INDEX) List<String> indexName, @QueryParam(LumongoConstants.QUERY) String query,
-					@QueryParam(LumongoConstants.AMOUNT) int amount, @QueryParam(LumongoConstants.FACET) List<String> facet,
-					@QueryParam(LumongoConstants.PRETTY) boolean pretty) {
-		
-		QueryRequest.Builder qrBuilder = QueryRequest.newBuilder().addAllIndex(indexName);
+	public String get(
+			@QueryParam(LumongoConstants.INDEX) List<String> indexName,
+			@QueryParam(LumongoConstants.QUERY) String query,
+			@QueryParam(LumongoConstants.QUERY_FIELD) List<String> queryFields,
+			@QueryParam(LumongoConstants.FILTER_QUERY) List<String> filterQueries,
+			@QueryParam(LumongoConstants.AMOUNT) int amount,
+			@QueryParam(LumongoConstants.FACET) List<String> facet,
+			@QueryParam(LumongoConstants.PRETTY) boolean pretty) {
+
+		QueryRequest.Builder qrBuilder = QueryRequest.newBuilder().addAllIndex(
+				indexName);
 		qrBuilder.setQuery(query);
 		qrBuilder.setAmount(amount);
+		
+		if (queryFields != null) {
+			for (String queryField : queryFields) {
+				qrBuilder.addQueryFields(queryField);
+			}
+		}
+		
+		if (filterQueries != null) {
+			for (String filterQuery : filterQueries) {
+				qrBuilder.addFilterQuery(filterQuery);
+			}
+		}
+		
 		FacetRequest.Builder frBuilder = FacetRequest.newBuilder();
 		for (String f : facet) {
 			CountRequest.Builder countBuilder = CountRequest.newBuilder();
-			//TODO handle path
+			// TODO handle path
 			LMFacet lmFacet = LMFacet.newBuilder().setLabel(f).build();
-			
+
 			frBuilder.addCountRequest(countBuilder.setFacetField(lmFacet));
 		}
 		qrBuilder.setFacetRequest(frBuilder);
-		
+
 		try {
 			QueryResponse qr = indexManager.query(qrBuilder.build());
-			
+
 			String response = JsonFormat.printToString(qr);
 			if (pretty) {
 				response = JsonWriter.formatJson(response);
 			}
 			return response;
+		} catch (Exception e) {
+			throw new WebApplicationException(LumongoConstants.UNIQUE_ID
+					+ " and " + LumongoConstants.FILE_NAME + " are required",
+					LumongoConstants.INTERNAL_ERROR);
 		}
-		catch (Exception e) {
-			throw new WebApplicationException(LumongoConstants.UNIQUE_ID + " and " + LumongoConstants.FILE_NAME + " are required",
-							LumongoConstants.INTERNAL_ERROR);
-		}
-		
+
 	}
 }
