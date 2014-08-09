6ba944fb53f2914af169812a0441b26ee130e360
Matt Davis <davis.matt.t@gmail.com>
2014-08-09 11:44:08 -0400

    make gradle happy with the javadoc

--- lumongo-cluster/src/main/java/org/lumongo/analyzer/StandardFoldingAnalyzer.java
+++ lumongo-cluster/src/main/java/org/lumongo/analyzer/StandardFoldingAnalyzer.java
@@ -25,74 +25,38 @@ import org.apache.lucene.analysis.core.LowerCaseFilter;
 import org.apache.lucene.analysis.core.StopAnalyzer;
 import org.apache.lucene.analysis.core.StopFilter;
 import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilter;
-import org.apache.lucene.analysis.standard.ClassicAnalyzer;
-import org.apache.lucene.analysis.standard.ClassicTokenizer;
 import org.apache.lucene.analysis.standard.StandardFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.analysis.util.CharArraySet;
 import org.apache.lucene.analysis.util.StopwordAnalyzerBase;
-import org.apache.lucene.analysis.util.WordlistLoader;
 import org.apache.lucene.util.Version;
 
-/**
- * Filters {@link StandardTokenizer} with {@link StandardFilter}, {@link
- * LowerCaseFilter} and {@link StopFilter}, using a list of
- * English stop words.
- *
- * <a name="version"/>
- * <p>You must specify the required {@link Version}
- * compatibility when creating StandardAnalyzer:
- * <ul>
- *   <li> As of 3.4, Hiragana and Han characters are no longer wrongly split
- *        from their combining characters. If you use a previous version number,
- *        you get the exact broken behavior for backwards compatibility.
- *   <li> As of 3.1, StandardTokenizer implements Unicode text segmentation,
- *        and StopFilter correctly handles Unicode 4.0 supplementary characters
- *        in stopwords.  {@link ClassicTokenizer} and {@link ClassicAnalyzer}
- *        are the pre-3.1 implementations of StandardTokenizer and
- *        StandardAnalyzer.
- *   <li> As of 2.9, StopFilter preserves position increments
- *   <li> As of 2.4, Tokens incorrectly identified as acronyms
- *        are corrected (see <a href="https://issues.apache.org/jira/browse/LUCENE-1068">LUCENE-1068</a>)
- * </ul>
- */
 public final class StandardFoldingAnalyzer extends StopwordAnalyzerBase {
-	
+
 	/** Default maximum allowed token length */
 	public static final int DEFAULT_MAX_TOKEN_LENGTH = 255;
-	
+
 	private int maxTokenLength = DEFAULT_MAX_TOKEN_LENGTH;
-	
+
 	/** An unmodifiable set containing some common English words that are usually not
 	useful for searching. */
 	public static final CharArraySet STOP_WORDS_SET = StopAnalyzer.ENGLISH_STOP_WORDS_SET;
-	
+
 	/** Builds an analyzer with the given stop words.
-	 * @param matchVersion Lucene version to match See {@link
-	 * <a href="#version">above</a>}
+	 * @param matchVersion Lucene version to match
 	 * @param stopWords stop words */
 	public StandardFoldingAnalyzer(Version matchVersion, CharArraySet stopWords) {
 		super(matchVersion, stopWords);
 	}
 	
-	/** Builds an analyzer with the default stop words ({@link
-	 * #STOP_WORDS_SET}).
-	 * @param matchVersion Lucene version to match See {@link
-	 * <a href="#version">above</a>}
-	 */
 	public StandardFoldingAnalyzer(Version matchVersion) {
 		this(matchVersion, STOP_WORDS_SET);
 	}
 	
-	/** Builds an analyzer with the stop words from the given reader.
-	 * @see WordlistLoader#getWordSet(Reader, Version)
-	 * @param matchVersion Lucene version to match See {@link
-	 * <a href="#version">above</a>}
-	 * @param stopwords Reader to read stop words from */
 	public StandardFoldingAnalyzer(Version matchVersion, Reader stopwords) throws IOException {
 		this(matchVersion, loadStopwordSet(stopwords, matchVersion));
 	}
-	
+
 	/**
 	 * Set maximum allowed token length.  If a token is seen
 	 * that exceeds this length then it is discarded.  This
@@ -102,14 +66,14 @@ public final class StandardFoldingAnalyzer extends StopwordAnalyzerBase {
 	public void setMaxTokenLength(int length) {
 		maxTokenLength = length;
 	}
-	
+
 	/**
 	 * @see #setMaxTokenLength
 	 */
 	public int getMaxTokenLength() {
 		return maxTokenLength;
 	}
-	
+
 	@Override
 	protected TokenStreamComponents createComponents(final String fieldName, final Reader reader) {
 		final StandardTokenizer src = new StandardTokenizer(matchVersion, reader);
