45bb585405dfe95373d21e07948a7b9fe8cdd96d
Matt Davis <davis.matt.t@gmail.com>
2014-12-02 17:17:31 -0500

    #34 use a platform independent way of detecting the working directory provided by @conorroche

--- lumongo-cluster/scripts/startnode.sh
+++ lumongo-cluster/scripts/startnode.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-WORKING_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+WORKING_DIR="${BASH_SOURCE[0]}";
+if ([ -h "${WORKING_DIR}" ]) then
+while([ -h "${WORKING_DIR}" ]) do WORKING_DIR=`readlink "${WORKING_DIR}"`; done
+fi
+pushd . > /dev/null
+cd `dirname ${WORKING_DIR}` > /dev/null
+WORKING_DIR=`pwd`;
+popd > /dev/null
 
 if [ -z "$LUMONGO_JAVA_SETTINGS" ]
 then
