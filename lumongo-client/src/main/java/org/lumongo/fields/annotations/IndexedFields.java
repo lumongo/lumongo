232193a4bf27b83e9ab94a26144940bc69351fbd
Matt Davis <davis.matt.t@gmail.com>
2015-02-09 23:11:44 -0500

    add ability to use multiple indexed anontations

--- lumongo-client/src/main/java/org/lumongo/fields/annotations/IndexedFields.java
+++ lumongo-client/src/main/java/org/lumongo/fields/annotations/IndexedFields.java
@@ -1,10 +1,14 @@
 package org.lumongo.fields.annotations;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+/**
+ * Specifics a field should be indexed multiple ways
+ *
+ *
+ */
+@Documented
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.FIELD })
 public @interface IndexedFields {
