ac4fb239a87b89f51b4796465cdd388ddb68c8c2
Matt Davis <davis.matt.t@gmail.com>
2015-02-22 17:55:22 -0500

    fix hazelcast serialization of updatesegmentstask

--- lumongo-cluster/src/main/java/org/lumongo/server/hazelcast/UpdateSegmentsTask.java
+++ lumongo-cluster/src/main/java/org/lumongo/server/hazelcast/UpdateSegmentsTask.java
@@ -1,25 +1,32 @@
 package org.lumongo.server.hazelcast;
 
-import java.io.Serializable;
+import com.hazelcast.core.Member;
+import com.hazelcast.instance.MemberImpl;
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.DataSerializable;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.Callable;
 
-import org.apache.log4j.Logger;
-
-import com.hazelcast.core.Member;
-
-public class UpdateSegmentsTask implements Callable<Void>, Serializable {
-	
-	private static Logger log = Logger.getLogger(UpdateSegmentsTask.class);
+public class UpdateSegmentsTask implements Callable<Void>, DataSerializable {
 	
 	private static final long serialVersionUID = 1L;
+	private static Logger log = Logger.getLogger(UpdateSegmentsTask.class);
+	private int hazelcastPort;
 	
-	private final int hazelcastPort;
-	
-	private final String indexName;
-	private final Map<Member, Set<Integer>> newMemberToSegmentMap;
-	
+	private String indexName;
+	private Map<Member, Set<Integer>> newMemberToSegmentMap;
+
+	public UpdateSegmentsTask() {
+
+	}
+
 	public UpdateSegmentsTask(int hazelcastPort, String indexName, Map<Member, Set<Integer>> newMemberToSegmentMap) {
 		this.hazelcastPort = hazelcastPort;
 		this.indexName = indexName;
@@ -37,5 +44,41 @@ public class UpdateSegmentsTask implements Callable<Void>, Serializable {
 			throw e;
 		}
 		return null;
+	}
+
+	@Override
+	public void writeData(ObjectDataOutput out) throws IOException {
+		out.writeInt(hazelcastPort);
+		out.writeUTF(indexName);
+		out.writeInt(newMemberToSegmentMap.size());
+
+		for (Map.Entry<Member, Set<Integer>> e : newMemberToSegmentMap.entrySet()) {
+			e.getKey().writeData(out);
+			out.writeInt(e.getValue().size());
+			for (Integer segment : e.getValue()) {
+				out.writeInt(segment);
+			}
+		}
+
+	}
+
+	@Override
+	public void readData(ObjectDataInput in) throws IOException {
+		hazelcastPort = in.readInt();
+		indexName = in.readUTF();
+		newMemberToSegmentMap = new HashMap<>();
+		int count = in.readInt();
+
+		for (int i = 0; i < count; i++) {
+			MemberImpl mi = new MemberImpl();
+			mi.readData(in);
+			Set<Integer> segments = new HashSet<>();
+			int segCount = in.readInt();
+			for (int j = 0; j < segCount; j++) {
+				segments.add(in.readInt());
+			}
+			newMemberToSegmentMap.put(mi, segments);
+		}
+
 	}
 }
