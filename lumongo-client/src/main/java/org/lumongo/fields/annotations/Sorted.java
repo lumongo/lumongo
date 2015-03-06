58228baa74872c1440c99128cca59bc723c19d5f
Matt Davis <davis.matt.t@gmail.com>
2015-03-05 21:43:33 -0500

    Start to fix the sort code for Lucene 5.0 #43

---
+++ lumongo-client/src/main/java/org/lumongo/fields/annotations/Sorted.java
@@ -0,0 +1,21 @@
+package org.lumongo.fields.annotations;
+
+import org.lumongo.cluster.message.Lumongo.FacetAs.LMFacetType;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD })
+public @interface Sorted {
+	
+	LMFacetType type() default LMFacetType.STANDARD;
+	
+	String name() default "";
+}
