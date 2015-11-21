5f009d2d38324b4b3efda761b0455983bd9a1438
Matt Davis <davis.matt.t@gmail.com>
2015-11-20 20:22:41 -0500

    move gridfs associated files code to use mongodb java driver 3.1's new syntax.  needs testing

--- lumongo-cluster/src/main/java/org/lumongo/storage/rawfiles/MongoDocumentStorage.java
+++ lumongo-cluster/src/main/java/org/lumongo/storage/rawfiles/MongoDocumentStorage.java
@@ -2,15 +2,18 @@ package org.lumongo.storage.rawfiles;
 
 import com.google.protobuf.ByteString;
 import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
+import com.mongodb.Block;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSBuckets;
+import com.mongodb.client.gridfs.GridFSFindIterable;
+import com.mongodb.client.gridfs.model.GridFSFile;
+import com.mongodb.client.gridfs.model.GridFSUploadOptions;
 import com.mongodb.client.model.UpdateOptions;
-import com.mongodb.gridfs.GridFS;
 import com.mongodb.gridfs.GridFSDBFile;
-import com.mongodb.gridfs.GridFSFile;
 import org.apache.log4j.Logger;
 import org.bson.BSON;
 import org.bson.BasicBSONObject;
@@ -24,11 +27,15 @@ import org.lumongo.storage.constants.MongoConstants;
 import org.lumongo.util.CommonCompression;
 import org.lumongo.util.CommonCompression.CompressionLevel;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 import java.util.zip.DeflaterInputStream;
 import java.util.zip.InflaterInputStream;
 
@@ -38,7 +45,6 @@ public class MongoDocumentStorage implements DocumentStorage {
 
 	private static final String ASSOCIATED_FILES = "associatedFiles";
 	private static final String FILES = "files";
-	private static final String FILENAME = "filename";
 	private static final String CHUNKS = "chunks";
 	private static final String ASSOCIATED_METADATA = "metadata";
 
@@ -46,6 +52,7 @@ public class MongoDocumentStorage implements DocumentStorage {
 	private static final String METADATA = "_meta_";
 	private static final String COMPRESSED_FLAG = "_comp_";
 	private static final String DOCUMENT_UNIQUE_ID_KEY = "_uid_";
+	private static final String FILE_UNIQUE_ID_KEY = "_fid_";
 
 	private MongoClient mongoClient;
 	private String database;
@@ -83,10 +90,10 @@ public class MongoDocumentStorage implements DocumentStorage {
 		adminDb.runCommand(shardCommand);
 	}
 
-	private GridFS createGridFSConnection() {
+	private GridFSBucket createGridFSConnection() {
 		//Grid FS does not use the new api yet as of java driver 3.0-beta-2
-		DB db = mongoClient.getDB(database);
-		return new GridFS(db, ASSOCIATED_FILES);
+		MongoDatabase db = mongoClient.getDatabase(database);
+		return GridFSBuckets.create(db, ASSOCIATED_FILES);
 	}
 
 	@Override
@@ -163,8 +170,8 @@ public class MongoDocumentStorage implements DocumentStorage {
 
 	@Override
 	public void deleteAllDocuments() {
-		GridFS gridFS = createGridFSConnection();
-		gridFS.remove(new BasicDBObject());
+		GridFSBucket gridFS = createGridFSConnection();
+		gridFS.drop();
 
 		MongoDatabase db = mongoClient.getDatabase(database);
 		MongoCollection<Document> coll = db.getCollection(rawCollectionName);
@@ -178,20 +185,23 @@ public class MongoDocumentStorage implements DocumentStorage {
 	}
 
 	@Override
-	public void storeAssociatedDocument(String uniqueId, String fileName, InputStream is, boolean compress, long timestamp, HashMap<String, String> metadataMap)
+	public void storeAssociatedDocument(String uniqueId, String fileName, InputStream is, boolean compress, long timestamp, Map<String, String> metadataMap)
 			throws Exception {
-		GridFS gridFS = createGridFSConnection();
+		GridFSBucket gridFS = createGridFSConnection();
 
 		if (compress) {
 			is = new DeflaterInputStream(is);
 		}
 
 		deleteAssociatedDocument(uniqueId, fileName);
-		GridFSFile gFile = gridFS.createFile(is);
-		gFile.put(MongoConstants.StandardFields._ID, getGridFsId(uniqueId, fileName));
-		gFile.put(FILENAME, fileName);
 
-		DBObject metadata = new BasicDBObject();
+		GridFSUploadOptions gridFSUploadOptions = getGridFSUploadOptions(uniqueId, fileName, compress, timestamp, metadataMap);
+		gridFS.uploadFromStream(fileName, is, gridFSUploadOptions);
+	}
+
+	private GridFSUploadOptions getGridFSUploadOptions(String uniqueId, String fileName, boolean compress, long timestamp,
+			Map<String, String> metadataMap) {
+		Document metadata = new Document();
 		if (metadataMap != null) {
 			for (String key : metadataMap.keySet()) {
 				metadata.put(key, metadataMap.get(key));
@@ -200,42 +210,37 @@ public class MongoDocumentStorage implements DocumentStorage {
 		metadata.put(TIMESTAMP, timestamp);
 		metadata.put(COMPRESSED_FLAG, compress);
 		metadata.put(DOCUMENT_UNIQUE_ID_KEY, uniqueId);
-		gFile.setMetaData(metadata);
-		gFile.save();
+		metadata.put(FILE_UNIQUE_ID_KEY, getGridFsId(uniqueId, fileName));
+
+		return new GridFSUploadOptions().chunkSizeBytes(1024).metadata(metadata);
 	}
 
 	@Override
 	public void storeAssociatedDocument(AssociatedDocument doc) throws Exception {
-		GridFS gridFS = createGridFSConnection();
 
 		byte[] bytes = doc.getDocument().toByteArray();
 		if (doc.getCompressed()) {
 			bytes = CommonCompression.compressZlib(bytes, CompressionLevel.FASTEST);
 		}
 
-		deleteAssociatedDocument(doc.getDocumentUniqueId(), doc.getFilename());
-		GridFSFile gFile = gridFS.createFile(bytes);
-		gFile.put(MongoConstants.StandardFields._ID, getGridFsId(doc.getDocumentUniqueId(), doc.getFilename()));
-		gFile.put(FILENAME, doc.getFilename());
-		DBObject metadata = new BasicDBObject();
+		ByteArrayInputStream byteInputStream = new ByteArrayInputStream(bytes);
+
+		Map<String, String> metadata = new HashMap<>();
 		for (Metadata meta : doc.getMetadataList()) {
 			metadata.put(meta.getKey(), meta.getValue());
 		}
-		metadata.put(TIMESTAMP, doc.getTimestamp());
-		metadata.put(COMPRESSED_FLAG, doc.getCompressed());
-		metadata.put(DOCUMENT_UNIQUE_ID_KEY, doc.getDocumentUniqueId());
-		gFile.setMetaData(metadata);
-		gFile.save();
+
+		storeAssociatedDocument(doc.getDocumentUniqueId(), doc.getFilename(), byteInputStream, doc.getCompressed(), doc.getTimestamp(), metadata);
 	}
 
 	@Override
 	public List<AssociatedDocument> getAssociatedDocuments(String uniqueId, FetchType fetchType) throws Exception {
-		GridFS gridFS = createGridFSConnection();
-		List<AssociatedDocument> assocDocs = new ArrayList<AssociatedDocument>();
+		GridFSBucket gridFS = createGridFSConnection();
+		List<AssociatedDocument> assocDocs = new ArrayList<>();
 		if (!FetchType.NONE.equals(fetchType)) {
-			List<GridFSDBFile> files = gridFS.find(new BasicDBObject(ASSOCIATED_METADATA + "." + DOCUMENT_UNIQUE_ID_KEY, uniqueId));
-			for (GridFSDBFile file : files) {
-				AssociatedDocument ad = loadGridFSToAssociatedDocument(file, fetchType);
+			GridFSFindIterable files = gridFS.find(new Document(ASSOCIATED_METADATA + "." + DOCUMENT_UNIQUE_ID_KEY, uniqueId));
+			for (GridFSFile file : files) {
+				AssociatedDocument ad = loadGridFSToAssociatedDocument(gridFS, file, fetchType);
 				assocDocs.add(ad);
 			}
 
@@ -249,18 +254,18 @@ public class MongoDocumentStorage implements DocumentStorage {
 
 	@Override
 	public InputStream getAssociatedDocumentStream(String uniqueId, String fileName) {
-		GridFS gridFS = createGridFSConnection();
-		GridFSDBFile file = gridFS.findOne(new BasicDBObject(MongoConstants.StandardFields._ID, getGridFsId(uniqueId, fileName)));
+		GridFSBucket gridFS = createGridFSConnection();
+		GridFSFile file = gridFS.find(new Document(ASSOCIATED_METADATA + "." + FILE_UNIQUE_ID_KEY, getGridFsId(uniqueId, fileName))).first();
 
 		if (file == null) {
 			return null;
 		}
 
-		InputStream is = file.getInputStream();
+		InputStream is = gridFS.openDownloadStream(file.getObjectId());;
 
-		DBObject metadata = file.getMetaData();
-		if (metadata.containsField(COMPRESSED_FLAG)) {
-			boolean compressed = (boolean) metadata.removeField(COMPRESSED_FLAG);
+		Document metadata = file.getMetadata();
+		if (metadata.containsKey(COMPRESSED_FLAG)) {
+			boolean compressed = (boolean) metadata.remove(COMPRESSED_FLAG);
 			if (compressed) {
 				is = new InflaterInputStream(is);
 			}
@@ -271,38 +276,42 @@ public class MongoDocumentStorage implements DocumentStorage {
 
 	@Override
 	public AssociatedDocument getAssociatedDocument(String uniqueId, String fileName, FetchType fetchType) throws Exception {
-		GridFS gridFS = createGridFSConnection();
+		GridFSBucket gridFS = createGridFSConnection();
 		if (!FetchType.NONE.equals(fetchType)) {
-			GridFSDBFile file = gridFS.findOne(new BasicDBObject(MongoConstants.StandardFields._ID, getGridFsId(uniqueId, fileName)));
+			GridFSFile file = gridFS
+					.find(new Document(ASSOCIATED_METADATA + "." + FILE_UNIQUE_ID_KEY, getGridFsId(uniqueId, fileName))).first();
 			if (null != file) {
-				return loadGridFSToAssociatedDocument(file, fetchType);
+				return loadGridFSToAssociatedDocument(gridFS, file, fetchType);
 			}
 		}
 		return null;
 	}
 
-	private AssociatedDocument loadGridFSToAssociatedDocument(GridFSDBFile file, FetchType fetchType) throws IOException {
+	private AssociatedDocument loadGridFSToAssociatedDocument(GridFSBucket gridFS, GridFSFile file, FetchType fetchType) throws IOException {
 		AssociatedDocument.Builder aBuilder = AssociatedDocument.newBuilder();
 		aBuilder.setFilename(file.getFilename());
-		DBObject metadata = file.getMetaData();
+		Document metadata = file.getMetadata();
 
 		boolean compressed = false;
-		if (metadata.containsField(COMPRESSED_FLAG)) {
-			compressed = (boolean) metadata.removeField(COMPRESSED_FLAG);
+		if (metadata.containsKey(COMPRESSED_FLAG)) {
+			compressed = (boolean) metadata.remove(COMPRESSED_FLAG);
 		}
 
-		long timestamp = (long) metadata.removeField(TIMESTAMP);
+		long timestamp = (long) metadata.remove(TIMESTAMP);
 
 		aBuilder.setCompressed(compressed);
 		aBuilder.setTimestamp(timestamp);
 
-		aBuilder.setDocumentUniqueId((String) metadata.removeField(DOCUMENT_UNIQUE_ID_KEY));
+		aBuilder.setDocumentUniqueId((String) metadata.remove(DOCUMENT_UNIQUE_ID_KEY));
 		for (String field : metadata.keySet()) {
 			aBuilder.addMetadata(Metadata.newBuilder().setKey(field).setValue((String) metadata.get(field)));
 		}
 
 		if (FetchType.FULL.equals(fetchType)) {
-			byte[] bytes = MongoConstants.Functions.readFileFromGridFS(file);
+
+			ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+			gridFS.downloadToStream(file.getObjectId(), byteArrayOutputStream);
+			byte[] bytes = byteArrayOutputStream.toByteArray();
 			if (null != bytes) {
 				if (compressed) {
 					bytes = CommonCompression.uncompressZlib(bytes);
@@ -316,26 +325,27 @@ public class MongoDocumentStorage implements DocumentStorage {
 
 	@Override
 	public List<String> getAssociatedFilenames(String uniqueId) throws Exception {
-		GridFS gridFS = createGridFSConnection();
-		ArrayList<String> fileNames = new ArrayList<String>();
-		List<GridFSDBFile> files = gridFS.find(new BasicDBObject(ASSOCIATED_METADATA + "." + DOCUMENT_UNIQUE_ID_KEY, uniqueId));
-		for (GridFSDBFile file : files) {
-			fileNames.add(file.getFilename());
-		}
+		GridFSBucket gridFS = createGridFSConnection();
+		ArrayList<String> fileNames = new ArrayList<>();
+		gridFS.find(new Document(ASSOCIATED_METADATA + "." + DOCUMENT_UNIQUE_ID_KEY, uniqueId))
+				.forEach((Consumer<com.mongodb.client.gridfs.model.GridFSFile>) gridFSFile -> fileNames.add(gridFSFile.getFilename()));
+
 		return fileNames;
 	}
 
 	@Override
 	public void deleteAssociatedDocument(String uniqueId, String fileName) {
-		GridFS gridFS = createGridFSConnection();
-		gridFS.remove(new BasicDBObject(MongoConstants.StandardFields._ID, getGridFsId(uniqueId, fileName)));
+		GridFSBucket gridFS = createGridFSConnection();
+		gridFS.find(new Document(ASSOCIATED_METADATA + "." + FILE_UNIQUE_ID_KEY, getGridFsId(uniqueId, fileName)))
+				.forEach((Block<com.mongodb.client.gridfs.model.GridFSFile>) gridFSFile -> gridFS.delete(gridFSFile.getObjectId()));
 
 	}
 
 	@Override
 	public void deleteAssociatedDocuments(String uniqueId) {
-		GridFS gridFS = createGridFSConnection();
-		gridFS.remove(new BasicDBObject(ASSOCIATED_METADATA + "." + DOCUMENT_UNIQUE_ID_KEY, uniqueId));
+		GridFSBucket gridFS = createGridFSConnection();
+		gridFS.find(new Document(ASSOCIATED_METADATA + "." + DOCUMENT_UNIQUE_ID_KEY, uniqueId))
+				.forEach((Block<com.mongodb.client.gridfs.model.GridFSFile>) gridFSFile -> gridFS.delete(gridFSFile.getObjectId()));
 	}
 
 }
