dbe2e1d39811b8b5ab7c2e217a85d92914077b57
Matt Davis <matt.davis@ascend-tech.us>
2016-12-31 13:17:54 -0500

    fix storage test with recursive delete of /tmp/fsdirectory before test

--- lumongo-storage/src/test/java/org/lumongo/test/storage/BasicStorageTest.java
+++ lumongo-storage/src/test/java/org/lumongo/test/storage/BasicStorageTest.java
@@ -34,6 +34,12 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -231,6 +237,22 @@ public class BasicStorageTest {
 		{
 			MongoClient mongo = new MongoClient(hostName);
 			DistributedDirectory d = new DistributedDirectory(new MongoDirectory(mongo, databaseName, STORAGE_TEST_INDEX));
+
+			Path directory = Paths.get("/tmp/fsdirectory");
+			Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+				@Override
+				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+					Files.delete(file);
+					return FileVisitResult.CONTINUE;
+				}
+
+				@Override
+				public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+					Files.delete(dir);
+					return FileVisitResult.CONTINUE;
+				}
+
+			});
 
 			File f = new File("/tmp/fsdirectory");
 			f.mkdirs();
