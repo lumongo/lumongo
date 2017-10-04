f9cbe90159b0f72e4d0494af54e87a7e7777ce12
Matt Davis <matt.davis@ascend-tech.us>
2017-10-04 11:36:09 -0400

    fix wildcard normalization after move to lucene 7.0

--- lumongo-cluster/src/main/java/org/lumongo/server/index/LumongoAnalyzerFactory.java
+++ lumongo-cluster/src/main/java/org/lumongo/server/index/LumongoAnalyzerFactory.java
@@ -54,24 +54,44 @@ public class LumongoAnalyzerFactory {
 
 			@Override
 			protected TokenStreamComponents createComponents(String fieldName) {
-				AnalyzerSettings.Tokenizer tokenizer = analyzerSettings.getTokenizer();
-				List<AnalyzerSettings.Filter> filterList = analyzerSettings.getFilterList();
 
+				AnalyzerSettings.Tokenizer tokenizer = analyzerSettings.getTokenizer();
 				Tokenizer src;
-				TokenStream tok;
-				TokenStream lastTok;
 				if (AnalyzerSettings.Tokenizer.KEYWORD.equals(tokenizer)) {
 					src = new KeywordTokenizer();
+				}
+				else if (AnalyzerSettings.Tokenizer.WHITESPACE.equals(tokenizer)) {
+					src = new WhitespaceTokenizer();
+				}
+				else if (AnalyzerSettings.Tokenizer.STANDARD.equals(tokenizer)) {
+					src = new StandardTokenizer();
+				}
+				else {
+					throw new RuntimeException("Unknown tokenizer type <" + tokenizer);
+				}
+
+				return new TokenStreamComponents(src, getFilteredStream(src));
+			}
+
+			@Override
+			protected TokenStream normalize(String fieldName, TokenStream in) {
+				return getFilteredStream(in);
+			}
+
+			private TokenStream getFilteredStream(TokenStream src) {
+				TokenStream tok;
+				TokenStream lastTok;
+
+				AnalyzerSettings.Tokenizer tokenizer = analyzerSettings.getTokenizer();
+				if (AnalyzerSettings.Tokenizer.KEYWORD.equals(tokenizer)) {
 					tok = src;
 					lastTok = src;
 				}
 				else if (AnalyzerSettings.Tokenizer.WHITESPACE.equals(tokenizer)) {
-					src = new WhitespaceTokenizer();
 					tok = src;
 					lastTok = src;
 				}
 				else if (AnalyzerSettings.Tokenizer.STANDARD.equals(tokenizer)) {
-					src = new StandardTokenizer();
 					tok = new StandardFilter(src);
 					lastTok = tok;
 				}
@@ -79,6 +99,7 @@ public class LumongoAnalyzerFactory {
 					throw new RuntimeException("Unknown tokenizer type <" + tokenizer);
 				}
 
+				List<AnalyzerSettings.Filter> filterList = analyzerSettings.getFilterList();
 				for (AnalyzerSettings.Filter filter : filterList) {
 					if (AnalyzerSettings.Filter.LOWERCASE.equals(filter)) {
 						tok = new LowerCaseFilter(lastTok);
@@ -148,10 +169,13 @@ public class LumongoAnalyzerFactory {
 					}
 					lastTok = tok;
 				}
-
-				return new TokenStreamComponents(src, tok);
+				return tok;
 			}
+
+
 		};
+
+
 
 	}
 
